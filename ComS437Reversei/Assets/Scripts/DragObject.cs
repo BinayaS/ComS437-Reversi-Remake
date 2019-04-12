3409fc51179ad8b9a9640dc8595b852fbb18cc90
CubedKing <BinayaShree@gmail.com>
2019-04-12 13:40:39 -0500

    update
    - updated game piece prefab
    - started work on board data representation

--- ComS437Reversei/Assets/Scripts/DragObject.cs
+++ ComS437Reversei/Assets/Scripts/DragObject.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class DragObject : MonoBehaviour
 {
+    public bool canPickup;
     private Vector3 mOffset;
     private float mZCoord;
+    public GameObject ghostPieceBlack;
+    public GameObject ghostPieceWhite;
+    public Vector3 originalPlace;
+    public MeshCollider physicsCollider;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        //canPickup = true;
+        ghostPieceBlack.transform.position = new Vector3(0f, -100f, 0f);
+        ghostPieceWhite.transform.position = new Vector3(0f, -100f, 0f);
+        physicsCollider = GetComponent<MeshCollider>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(!canPickup)
+        {
+            rb.useGravity = false;
+            physicsCollider.enabled = false;
+        } else
+        {
+            rb.useGravity = true;
+            physicsCollider.enabled = true;
+        }
+    }
 
     Vector3 GetMouseWorldPos() {
         Vector3 mousePoint = Input.mousePosition;
@@ -14,22 +42,47 @@ public class DragObject : MonoBehaviour
     }
 
     private void OnMouseDown() {
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        mOffset = gameObject.transform.position - GetMouseWorldPos();
-        PieceValueController.isPickedUp = true;
+        if (canPickup == true)
+        {
+            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+            mOffset = gameObject.transform.position - GetMouseWorldPos();
+            PieceValueController.isPickedUp = true;
+            originalPlace = gameObject.transform.position;
+        }
     }
 
     private void OnMouseUp()
     {
-        PieceValueController.isPickedUp = false;
+        if(canPickup == true)
+        {
+            PieceValueController.isPickedUp = false;
+            if (ghostPieceBlack.transform.position == new Vector3(0f, -100f, 0f))
+            {
+                gameObject.transform.position = originalPlace;
+            }
+            else
+            {
+                gameObject.transform.position = ghostPieceBlack.transform.position;
+            }
+
+            ghostPieceBlack.transform.position = new Vector3(0f, -100f, 0f);
+            ghostPieceWhite.transform.position = new Vector3(0f, -100f, 0f);
+        }
     }
 
     private void OnMouseDrag() {
-        gameObject.transform.position = GetMouseWorldPos() + mOffset;
+        if (canPickup == true)
+        {
+            gameObject.transform.position = GetMouseWorldPos() + mOffset;
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        ShowGhostPiece.spawnedPiece.transform.position = gameObject.transform.position;
+        if(other.name != "Model_Board" && other.name != "Plane" && canPickup == true)
+        {
+            ghostPieceBlack.transform.position = other.transform.position;
+        }
     }
 }
